1aa1a708f3b451262fe28503debc41e36cad8c09
alexandrubusila <alexandrubusila@gmail.com>
2015-04-06 16:18:41 -0400

    Fixed n3 lines

--- client/src/js/app/app.js
+++ client/src/js/app/app.js
@@ -26,42 +26,57 @@ define([
               "$scope",
               "$http",
               function ($scope, $http) {
-                $http.get('/current_status').
-                    success(function(data, status, headers, config) {
-                        // this callback will be called asynchronously
-                        // when the response is available
-                        $scope.message = data;
-                    }).
-                    error(function(data, status, headers, config) {
-                        // called asynchronously if an error occurs
-                        // or server returns response with an error status.
-                        console.log('Error - GET on current_status, status: '+status);
-                    });
 
-                $http.get('/temp1_graph_data').
-                    success(function(data, status, headers, config) {
-                        // this callback will be called asynchronously
-                        // when the response is available
-                        $scope.data = data.slave1.data;
-                    }).
-                    error(function(data, status, headers, config) {
-                        // called asynchronously if an error occurs
-                        // or server returns response with an error status.
-                        console.log('Error - GET on current_status, status: '+status);
-                    });
+                $http.get('/current_status')
+                  .success(function(data, status, headers, config) {
+                    // this callback will be called asynchronously
+                    // when the response is available
+                    $scope.message = data;
+                  })
+                  .error(function(data, status, headers, config) {
+                    // called asynchronously if an error occurs
+                    // or server returns response with an error status.
+                    console.log('Error - GET on current_status, status: '+status);
+                  });
+
+                $http.get('/temp1_graph_data')
+                  .success(function(data, status, headers, config) {
+                    // this callback will be called asynchronously
+                    // when the response is available
+                    // TODO: Separate concern in servide to isolate transformation logic
+                    for (var i = 0, _len = data.slave1.data.length; i < _len; ++i) {
+                      data.slave1.data[i].x = new Date(data.slave1.data[i].x);
+                    }
+                    $scope.data = data.slave1.data;
+                  })
+                  .error(function(data, status, headers, config) {
+                    // called asynchronously if an error occurs
+                    // or server returns response with an error status.
+                    console.log('Error - GET on current_status, status: '+status);
+                  });
 
                 $scope.options = {
-                    axes: {
-                        x: {key: 'x', type: 'date'},
-                        y: {type: 'linear'}
+                  axes: {
+                    x: {
+                      type: "date",
+                      key: "x"
                     },
-                    series: [
-                        {y: 'value', color: 'steelblue', label: 'Temp1 slave1', lineMode: 'dashed'}
-                    ],
-                    tooltip: {
-                        mode: 'scrubber'
+                    y: {
+                      type: "linear"
                     }
-                }
+                  },
+                  series: [
+                    {
+                      y: "value",
+                      color: "steelblue",
+                      label: "Temp slave1",
+                      lineMode: "dashed"
+                    }
+                  ],
+                  tooltip: {
+                    mode: "scrubber"
+                  }
+                };
             }]
           }
         }
